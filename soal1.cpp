b3abd5a014823ae63af75e1c24e5cb4c8f36e404
Rizkimuda <rizkisiregar809@gmail.com>
2025-11-03 08:20:58 +0700

    Implement left rotation of array in C++

--- soal1.cpp
+++ soal1.cpp
@@ -1,1 +1,40 @@
-//Masukkan jawaban disini
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> rotasiKiri(int d, vector<int> arr) {
+    int n = arr.size();
+    d = d % n;
+    
+    vector<int> hasil(n);
+
+    for (int i = 0; i < n; i++) {
+        int posisiBaru = (i + n - d) % n;
+        hasil[posisiBaru] = arr[i];
+    }
+
+    return hasil;
+}
+
+int main() {
+    int n, d;
+
+    cout << "Masukkan jumlah elemen array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Masukkan elemen array: ";
+    for (int i = 0; i < n; i++){ 
+        cin >> arr[i]; 
+    }
+
+    cout << "Masukkan jumlah rotasi: ";
+    cin >> d;
+
+    vector<int> hasil = rotasiKiri(d, arr);
+
+    cout << "Hasil rotasi: ";
+    for (int x : hasil) cout << x << " ";
+
+    return 0;
+}
